1060204188d61589ae1d9f2e49f9d7c8b8de5206
Ejedavy <72175818+Ejedavy@users.noreply.github.com>
2022-01-30 00:22:49 +0300

    Update String.h

--- String.h
+++ String.h
@@ -20,15 +20,19 @@ namespace MyString {
         void displayName() const;
         int getLength() const;
         const char* getString() const;
+        String& operator=(const String& rhs);
+        String& operator=(String&& rhs);
     };
 
 #endif //C_STRING_H
 
+    //DEFAULT CONSTRUCTOR
     String::String():str{nullptr}{
         str = new char[1];
         *str = '\0';
     }
 
+    //THE CONSTRUCTOR OVERLOAD WITH ONE ARGUMENT
     String:: String(const char* s){
         if(s == nullptr){
             str = new char[1];
@@ -39,10 +43,54 @@ namespace MyString {
         }
     }
 
-    String::String(const String& source){
-
+    //THIS IS THE COPY CONSTRUCTOR OF THE STRING CLASS
+    String::String(const String& source):str{nullptr}{
+        str = new char[strlen(source.str) + 1];
+        std::strcpy(newString,source.str);
     }
 
+    //DESTRUCTOR
+    String::~String(){
+        delete[] str;
+    }
 
+    //DISPLAY METHOD
+    void String::displayName() const {
+        std::cout << str << ":" << getLength() << std::endl;
+    }
+
+    //Length Getter
+    int String::getLength() const {
+        return std::strlen(str);
+    }
+
+    //String Getter
+    const char* String::getString() const {
+        return str;
+    }
+
+    //OVERLOADING THE ASSIGNMENT OPERATOR (COPY ASSIGNMENT)
+    String& String::operator=(const String &rhs) {
+        if(this == &rhs){
+            return *this;
+        }else{
+            delete[] str;
+            str = new char[std::strlen(rhs) +1];
+            std::strcpy(str,rhs);
+            return *this;
+        }
+    }
+
+    //OVERLOADING THE ASSIGNMENT OPERATOR ( MOVE ASSIGNMENT ) **FOR LITERALS AND R-TYPE VALUES**
+    String& String::operator=(String&& rhs){
+        if(this == &rhs){
+            return *this;
+        }else{
+            delete [] str;
+            str = rhs.str;
+            rhs.str = nullptr;
+            return *this;
+        }
+    }
 }
 
